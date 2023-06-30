739a8512fc0b31639a43d64ad0d59cf8f73d4d11
msa-akeller <136749794+msa-akeller@users.noreply.github.com>
2023-06-30 14:24:27 -0500

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+#scraper.py - OLD
+
+def main():
+    symbol = "AAPL"
+    url = "https://finance.yahoo.com/quote/AAPL?p=AAPL&.tsrc=fin-srch"
+    headers = {"User-Agent": "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"}
+
+    #request page
+    response = requests.get(url, headers=headers)
+
+    #print(response.text) - OLD
+
+    #parse html using Beautiful Soup
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    stock_dictionary = {}
+    counter = 1
+    for cell in soup.find_all('td'):
+        if counter % 2 != 0:
+            key = cell.text
+        else:
+            value = cell.text
+            stock_dictionary[key] = value
+        
+        counter += 1
+
+    print(stock_dictionary)
+
+
+
+main()
